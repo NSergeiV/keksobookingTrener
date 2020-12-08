90079d04b20097d75ee70baf5c6573b8e26ddb1b
NSergeiV <ttniseva@yandex.ru>
2020-12-08 07:11:53 +0300

    Продолжаю изучать и пробовать техзадание

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 'use strict';
 
 let map = document.querySelector('.map');
-map.classList.remove('map--faded');
+// map.classList.remove('map--faded');
+
+// Раздел перемещения метки указания адреса объявления
+const mapPinMain = map.querySelector('.map__pin--main');
+console.log(mapPinMain);
