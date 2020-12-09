0620b0d6130ebfd37ffbf47f0c730a6f6136d9ea
NSergeiV <ttniseva@yandex.ru>
2020-12-10 00:08:41 +0300

    Разобрался с меткой и ее функционалом, теперь нужно почистить код

--- js/main.js
+++ js/main.js
@@ -1,13 +1,51 @@
 'use strict';
 
-// let map = document.querySelector('.map');
-// map.classList.remove('map--faded');
+let map = document.querySelector('.map');
+
 
 // Раздел перемещения метки указания адреса объявления
 let mapPinMain = document.querySelector('.map__pin--main');
-let kog = mapPinMain.querySelector('svg');
+let markerSvg = mapPinMain.querySelector('svg');
+let markerImg = mapPinMain.querySelector('img');
 
-kog.addEventListener('mousedown', function (evt) {
+// Активируем внутренний маркер
+markerImg.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  map.classList.remove('map--faded');
+
+  let startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  let onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    let shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+    mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+  };
+
+  let onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+// Активируем наружную часть маркера
+markerSvg.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
 
   let startCoords = {
