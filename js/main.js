8327d566adeb3dd0331402c4f6097e043e33d9a1
NSergeiV <ttniseva@yandex.ru>
2020-12-11 01:21:17 +0300

    Отредактировал код, убрал дублирование кода. Установил блокировку интерактивных элементов при загрузке страницы и снятия блокировки при взаимодействии с метрой.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 'use strict';
 
+// РАЗДЕЛ блокировки интерактивных элементов формы
+const adForm = document.querySelector('.ad-form');
+const fieldsets = adForm.querySelectorAll('fieldset');
+for (let i = 0; i < fieldsets.length; i++) {
+  fieldsets[i].setAttribute('disabled', '');
+}
+// КОНЕЦ РАЗДЕЛА
+
+// РАЗДЕЛ перемещения метки указания адреса объявления
 let map = document.querySelector('.map');
-
-
-// Раздел перемещения метки указания адреса объявления
 let mapPinMain = document.querySelector('.map__pin--main');
 let markerSvg = mapPinMain.querySelector('svg');
 let markerImg = mapPinMain.querySelector('img');
 
-// Активируем внутренний маркер
-markerImg.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
-  map.classList.remove('map--faded');
-
+// Функция перемещения
+const moving = (evt) => {
   let startCoords = {
     x: evt.clientX,
     y: evt.clientY
@@ -42,40 +45,23 @@ markerImg.addEventListener('mousedown', function (evt) {
   };
   document.addEventListener('mousemove', onMouseMove);
   document.addEventListener('mouseup', onMouseUp);
+};
+
+// Активируем внутренний маркер
+markerImg.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  map.classList.remove('map--faded');
+  for (let i = 0; i < fieldsets.length; i++) {
+    fieldsets[i].removeAttribute('disabled');
+  }
+
+  moving(evt);
 });
 
 // Активируем наружную часть маркера
 markerSvg.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
 
-  let startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-
-  let onMouseMove = function (moveEvt) {
-    moveEvt.preventDefault();
-
-    let shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
-    };
-
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
-
-    mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
-    mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
-  };
-
-  let onMouseUp = function (upEvt) {
-    upEvt.preventDefault();
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-  };
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
+  moving(evt);
 });
-// КОНУЦ РАЗДЕЛА
+// КОНЕЦ РАЗДЕЛА
