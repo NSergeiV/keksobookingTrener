d0371220c6cfc26a7293c8b04d3e2bb2bc640b92
NSergeiV <ttniseva@yandex.ru>
2021-01-13 02:05:38 +0300

    Добавили js файл и разнесли код по файлам, изменили код в index.

--- js/main.js
+++ js/main.js
@@ -4,93 +4,13 @@
 const adForm = document.querySelector('.ad-form');
 const fieldsets = adForm.querySelectorAll('fieldset');
 const adFormAddress = adForm.querySelector('input[name="address"]');
-console.log(adFormAddress);
+
+window.adFormAddress = adFormAddress;
+window.fieldsets = fieldsets;
+window.adForm = adForm;
 
 for (let i = 0; i < fieldsets.length; i++) {
   fieldsets[i].setAttribute('disabled', '');
 }
 // КОНЕЦ РАЗДЕЛА
 
-// РАЗДЕЛ перемещения метки указания адреса объявления
-let map = document.querySelector('.map');
-let mapPinMain = document.querySelector('.map__pin--main');
-let markerSvg = mapPinMain.querySelector('svg');
-let markerImg = mapPinMain.querySelector('img');
-const mapArea = map.querySelector('.map__pins');
-
-// Функция перемещения
-const moving = (evt, block) => {
-  const sizeWidthMap = mapArea.offsetWidth;
-  let startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-
-  const leftCoordMapArea = Math.floor(mapArea.getBoundingClientRect().left);
-  const topCoordMapArea = Math.floor(mapArea.getBoundingClientRect().top);
-
-  let centralPointX = (Math.ceil(block.getBoundingClientRect().left) + (Math.ceil(block.getBoundingClientRect().width) / 2)) - evt.pageX;
-  let centralPointY = (Math.ceil(block.getBoundingClientRect().top) + (Math.ceil(block.getBoundingClientRect().height) / 2)) - evt.pageY;
-  let centralPointYduble = (Math.ceil(block.getBoundingClientRect().top) + (Math.ceil(block.getBoundingClientRect().height) / 2)) - evt.offsetY;
-  console.log(centralPointY);
-  console.log(centralPointYduble);
-  let scopeSearch = {
-    leftX: leftCoordMapArea,
-    rightX: leftCoordMapArea + sizeWidthMap,
-    topY: topCoordMapArea + 130,
-    bottomY: (630 + topCoordMapArea) + topCoordMapArea
-  };
-
-  console.log(scopeSearch.topY);
-  console.log(scopeSearch.bottomY);
-
-  let onMouseMove = function (moveEvt) {
-    moveEvt.preventDefault();
-
-    console.log(moveEvt.clientY + centralPointY);
-
-    if (moveEvt.clientX + centralPointX >= scopeSearch.leftX && moveEvt.clientX + centralPointX < scopeSearch.rightX && moveEvt.clientY + centralPointY >= scopeSearch.topY && moveEvt.clientY + centralPointY < scopeSearch.bottomY) {
-      let shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
-      mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
-      adFormAddress.value = ((mapPinMain.offsetLeft - shift.x) + centralPointX) + ' ' + ((mapPinMain.offsetTop - shift.y) + centralPointY);
-    }
-  };
-
-  let onMouseUp = function (upEvt) {
-    upEvt.preventDefault();
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-  };
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-};
-
-// Активируем внутренний маркер
-markerImg.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
-  map.classList.remove('map--faded');
-  for (let i = 0; i < fieldsets.length; i++) {
-    fieldsets[i].removeAttribute('disabled');
-  }
-  adForm.classList.remove('ad-form--disabled');
-
-  moving(evt, markerImg);
-});
-
-// Активируем наружную часть маркера
-markerSvg.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
-
-  moving(evt, markerSvg);
-});
-// КОНЕЦ РАЗДЕЛА
---
+++ js/slider.js
@@ -0,0 +1,104 @@
+'use strict';
+
+(function () {
+  // РАЗДЕЛ перемещения метки с указанием координат в поле адреса объявления
+  let map = document.querySelector('.map');
+  let mapPinMain = document.querySelector('.map__pin--main');
+  let markerSvg = mapPinMain.querySelector('svg');
+  let markerImg = mapPinMain.querySelector('img');
+  const mapArea = map.querySelector('.map__pins');
+
+  window.adFormAddress.value = (mapPinMain.offsetLeft + Math.ceil(65 / 2)) + ' ' + (mapPinMain.offsetTop + Math.ceil(65 / 2));
+
+  // Функция перемещения
+  const moving = (evt, block) => {
+    const sizeWidthMap = mapArea.offsetWidth;
+    let startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    const leftCoordMapArea = Math.floor(mapArea.getBoundingClientRect().left);
+    const topCoordMapArea = Math.floor(mapArea.getBoundingClientRect().top);
+
+    let centralPointX = (Math.ceil(block.getBoundingClientRect().left) + (Math.ceil(block.getBoundingClientRect().width) / 2)) - evt.pageX;
+    let centralPointY;
+    let indent;
+    if (block === markerSvg) {
+      centralPointY = (Math.ceil(block.getBoundingClientRect().top) + (Math.ceil(block.getBoundingClientRect().height) / 2)) - evt.pageY;
+      indent = Math.ceil(block.getBoundingClientRect().height / 2);
+    } else {
+      centralPointY = (Math.ceil(block.getBoundingClientRect().top) + 82) - evt.pageY;
+      indent = 82;
+    }
+
+    let topLimit;
+
+    if (window.scrollY <= 130) {
+      topLimit = topCoordMapArea + (130 + indent) - window.scrollY;
+    } else {
+      topLimit = -(window.scrollY - indent);
+    }
+
+    let scopeSearch = {
+      leftX: leftCoordMapArea,
+      rightX: leftCoordMapArea + sizeWidthMap,
+      topY: topLimit,
+      // topY: topCoordMapArea + 130 - window.scrollY,
+      bottomY: (630 + 82 + topCoordMapArea) + topCoordMapArea
+    };
+
+    let onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      if (moveEvt.clientX + centralPointX >= scopeSearch.leftX && moveEvt.clientX + centralPointX < scopeSearch.rightX && moveEvt.clientY + centralPointY >= scopeSearch.topY && moveEvt.clientY + centralPointY < scopeSearch.bottomY) {
+        let shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY
+        };
+
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+
+        mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+        mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+
+        if (block === markerSvg) {
+          window.adFormAddress.value = ((mapPinMain.offsetLeft - shift.x) + Math.ceil(65 / 2)) + ' ' + ((mapPinMain.offsetTop - shift.y) + Math.ceil(65 / 2));
+        } else {
+          window.adFormAddress.value = ((mapPinMain.offsetLeft - shift.x) + Math.ceil(65 / 2)) + ' ' + ((mapPinMain.offsetTop - shift.y) + 82);
+        }
+      }
+    };
+
+    let onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  };
+
+  // Активируем внутренний маркер
+  markerImg.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    map.classList.remove('map--faded');
+    for (let i = 0; i < window.fieldsets.length; i++) {
+      window.fieldsets[i].removeAttribute('disabled');
+    }
+    window.adForm.classList.remove('ad-form--disabled');
+
+    moving(evt, markerImg);
+  });
+
+  // Активируем наружную часть маркера
+  markerSvg.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    moving(evt, markerSvg);
+  });
+  // КОНЕЦ РАЗДЕЛА
+})();
